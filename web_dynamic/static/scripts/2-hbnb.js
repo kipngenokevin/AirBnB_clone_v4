2860d308a601352f13663483e6b68a7b161eb8f5
kipngeno kevin <kipngenokevin254@gmail.com>
2024-03-02 17:33:26 +0000

    Task 4 changes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,17 +8,17 @@ $(() => {
 
   const updateAPIStatus = () => {
     $.ajax({
-	    url: 'http://0.0.0.0:5001/api/v1/status/',
-	    success: function(response) {
-              if (response.status === 'OK' ) {
-	        $('#api_status').addClass('available');
-	      } else {
-                $('#api_status').removeClass('available');
-	      }
-	    },
-	    error: function() {
-	      $('#api_status').removeClass('available');
-	    }
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      success: function (response) {
+        if (response.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      },
+      error: function () {
+        $('#api_status').removeClass('available');
+      }
     });
   };
 
---
+++ web_dynamic/static/scripts/3-hbnb-copy.js
@@ -0,0 +1,61 @@
+$(() => {
+  const amenityIds = {};
+
+  const updateAmenitiesList = () => {
+    const checkedAmenities = Object.values(amenityIds);
+    $('div.amenities h4').text(checkedAmenities.join(', '));
+  };
+
+  const updateAPIStatus = () => {
+    $.ajax({
+	    url: 'http://192.168.33.10:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+	    success: function (response) {
+        if (response.status === 'OK') {
+	        $('#api_status').addClass('available');
+          $('section.places').empty();
+          response.forEach(place => {
+            const placeHTML = ` 
+                      <article>
+                        <div class="title_box">
+                          <h2>${place.name}</h2>
+                          <div class="price_by_night">$${place.price_by_night}</div>
+                        </div>
+                        <div class="information">
+                          <div class="max_guest">${place.max_guest} Guest(s)</div>
+                          <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+                          <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+                        </div>
+                        <div class="description">${place.description}</div>
+                        </article>`;
+            $('section.places').append(placeHTML);
+          });
+	      } else {
+          $('#api_status').removeClass('available');
+	      }
+	    },
+	    error: function () {
+	      $('#api_status').removeClass('available');
+	    }
+    });
+  };
+
+  updateAPIStatus();
+
+  setInterval(updateAPIStatus, 10000);
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenityIds[amenityId] = amenityName;
+    } else {
+      delete amenityIds[amenityId];
+    }
+
+    updateAmenitiesList();
+  });
+});
